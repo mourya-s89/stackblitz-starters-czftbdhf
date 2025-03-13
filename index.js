e4cde91cfcca463b791bf88e8ccda82a6b0cdab4
mourya-s89 <kadali.mourya.s89@kalvium.community>
2025-03-13 12:16:44 +0530

    files

--- index.js
+++ index.js
@@ -34,18 +34,42 @@
 
 const express = require('express');
 const { resolve } = require('path');
+const data = require('./data.json');
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+
+app.post('/students/above-threshold',(req, res) => {
+  try{
+    const {threshold} = req.body;
+    if(!threshold){
+      return res.status(500).send({msg:"please provide threshold"})
+    }
+    const studentData = data.map((ele) => {
+      return {name: ele.name,total: ele.total};
+    }).filter((data)=>{
+      return data.total > threshold;
+    })
+
+    return res.status(200).send({count: studentData.length,students: studentData})
+
+
+  }catch(err){
+    return res.status(500).send({msg:"something went wrong",err})
+
+  }
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
